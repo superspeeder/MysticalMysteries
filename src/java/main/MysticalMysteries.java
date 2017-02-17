0f4bac26470a1936b7fe0d0f0483a9e4da3fc078
superspeeder <andy@delusion.org>
2017-02-17 14:32:53 -0800

    Update MysticalMysteries.java
    complete mod annotation

--- src/java/main/MysticalMysteries.java
+++ src/java/main/MysticalMysteries.java
@@ -2,4 +2,7 @@ package superspeeder.mysticalmysteries
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid
+
+@Mod(modid=Reference.modid, name=Reference.modname, version=Reference.modversion)
+public class MysticalMysteries {
+  public comp
